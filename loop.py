d59155ba443bc90cc863b77947a1a9c3f1868a08
codedreammer <nkanal38@gmail.com>
2024-07-09 12:46:14 +0530

    learn about loop

---
+++ loop.py
@@ -0,0 +1,33 @@
+#name = 'akshay'
+#for i in name:
+ #   print(i)
+   # if(i=='b'):
+ #       print("This is somthing spacial!")
+        
+
+colour = ["red","green","blue","yellow"]
+for colour in colour:
+    print(colour)
+    for i in colour:
+       print(i)    
+
+fruits = ["apple", "banana", "cherry"]
+for x in fruits:
+  print(x)
+  if x == "banana":
+    break
+#it will break the loop after the x = banana 
+  
+for k in range(5):
+   print(k+1)
+   
+#for k in range(1,20001):   
+ # print(k)
+  
+for x in range(2, 30, 3):
+  print(x)
+
+
+
+
+       
