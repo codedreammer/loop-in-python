b218da4863e6d817794b7ac01133b2f9099ccb52
codedreammer <nkanal38@gmail.com>
2024-07-10 19:07:30 +0530

    add file

--- while loop.py
+++ while loop.py
@@ -20,5 +20,12 @@ while i < 6:
   i += 1
 else:
   print("i is no longer less than 6")
-  
-  
+
+
+  #else in while loop
+i = 1
+while i < 6:
+  print(i)
+  i += 1
+else:
+  print("i is no longer less than 6") 
